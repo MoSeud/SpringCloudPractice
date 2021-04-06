e5c5d6329b0aa02501b03c5408f9d60d59126dfc
MoSeud <mseud52@gmail.com>
2021-04-05 23:29:43 -0400

    Initial commit

---
+++ src/main/java/com/cloud/springcloudpractice/SpringcloudpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.springcloudpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudpracticeApplication.class, args);
+    }
+
+}
